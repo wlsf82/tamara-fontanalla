894ead8670ef8417e06e3e9d10845b1dbbeddc0c
tamarafontanella <tamaraafontanella@gmail.com>
2019-06-10 22:23:33 -0300

    Criação dos testes de lista de usuário.
    Adicionado:
    Teste 1- Alteração de um email (porém existe dúvida na seleção do elemento em tela);
    Teste 2-Teste de exclusão de usuário da lista;

--- cypress/integration/Userlist.spec.js
+++ cypress/integration/Userlist.spec.js
@@ -1,10 +1,27 @@
 describe("User List", () => {
     beforeEach(() => cy.visit("/users"));
     
-    it("change first user's email", () => {
-        cy.get(".material-icons").first().click();
+//    it("change first user's email", () => {
+//        cy.get(".material-icons").first().click();
 
         // @TODO: implement the rest.
+//    });
+
+    it("change first user's email", () => {
+     // Como selecionar o segundo item de edição, com o ícone lápis? o first é a lupa.
+     //   cy.get("[href='/users/8552/edit']").click();
+        cy.get("#user_email").clear();
+        const ChangeWithEmail = {
+            email: "foo@bar.baz"
+        };
+
+        cy.AlterUser(ChangeWithEmail);
+
+        cy.get("#notice").should("contain", "Seu Usuário foi Atualizado!");
+    });
+    it("delete first user", () => {
+        cy.get("[data-method='delete']").first().click();
+        cy.get("#notice").should("contain", "Seu Usuário foi removido com sucesso!");
     });
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,3 +44,33 @@ Cypress.Commands.add("createUser", user => {
   }
   cy.get("input[type='submit']").click();
 });
+
+Cypress.Commands.add("AlterUser", change => {
+  if (change.name) {
+    cy.get("#user_name").type(change.firstName);
+  }
+  if (change.lastname) {
+    cy.get("#user_lastname").type(change.lastName);
+  }
+  if (change.email) {
+    cy.get("#user_email").type(change.email);
+  }
+  if (change.address) {
+    cy.get("#user_address").type(change.address);
+  }
+  if (change.university) {
+    cy.get("#user_university").type(change.university);
+  }
+  if (change.profile) {
+    cy.get("#user_profile").type(change.profile);
+  }
+  if (change.gender) {
+    cy.get("#user_gender").type(change.gender);
+  }
+  if (change.age) {
+    cy.get("#user_age").type(change.age);
+  }
+  cy.get("input[type='submit']").click();
+
+
+});
