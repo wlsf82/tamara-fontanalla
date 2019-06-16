dde20736e1f87de9165890331fe0f7c3b7809d1e
Walmyr Filho <limaesilva@qelp.com>
2019-06-16 19:20:12 +0200

    Remove duplicated code on custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,6 +27,25 @@ Cypress.Commands.add("createUser", user => {
   cy.get("#user_name").type(user.firstName);
   cy.get("#user_lastname").type(user.lastName);
   cy.get("#user_email").type(user.email);
+  cy.fillOptionalFields(user);
+  cy.get("input[type='submit']").click();
+});
+
+Cypress.Commands.add("AlterUser", user => {
+  if (user.name) {
+    cy.get("#user_name").type(user.firstName);
+  }
+  if (user.lastname) {
+    cy.get("#user_lastname").type(user.lastName);
+  }
+  if (user.email) {
+    cy.get("#user_email").type(user.email);
+  }
+  cy.fillOptionalFields(user);
+  cy.get("input[type='submit']").click();
+});
+
+Cypress.Commands.add("fillOptionalFields", user => {
   if (user.address) {
     cy.get("#user_address").type(user.address);
   }
@@ -42,35 +61,4 @@ Cypress.Commands.add("createUser", user => {
   if (user.age) {
     cy.get("#user_age").type(user.age);
   }
-  cy.get("input[type='submit']").click();
-});
-
-Cypress.Commands.add("AlterUser", change => {
-  if (change.name) {
-    cy.get("#user_name").type(change.firstName);
-  }
-  if (change.lastname) {
-    cy.get("#user_lastname").type(change.lastName);
-  }
-  if (change.email) {
-    cy.get("#user_email").type(change.email);
-  }
-  if (change.address) {
-    cy.get("#user_address").type(change.address);
-  }
-  if (change.university) {
-    cy.get("#user_university").type(change.university);
-  }
-  if (change.profile) {
-    cy.get("#user_profile").type(change.profile);
-  }
-  if (change.gender) {
-    cy.get("#user_gender").type(change.gender);
-  }
-  if (change.age) {
-    cy.get("#user_age").type(change.age);
-  }
-  cy.get("input[type='submit']").click();
-
-
-});
+})
