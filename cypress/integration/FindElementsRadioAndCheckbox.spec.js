8596585df238a1023a9474c87bf981591c53a2a4
Walmyr Filho <limaesilva@qelp.com>
2019-06-16 19:19:19 +0200

    Fix tests that interact with radio buttons and checboxes

--- cypress/integration/FindElementsRadioAndCheckbox.spec.js
+++ cypress/integration/FindElementsRadioAndCheckbox.spec.js
@@ -1,14 +1,11 @@
 describe("Find elements Radio-button and Check-box", () => {
     beforeEach(() => cy.visit("/buscaelementos/radioecheckbox"));
+
     it("Find radio button", () => {
-    //selecionar o radio button red, não funcionou
-        cy.get("[type='radio']").first().check('Red');
+        cy.get("[for='red']").click()
     });
 
-    it("Find check-box", () => {
-        cy.get("[type='checkbox']  [for='purple']").check();
-    //    cy.get("#purple").first().check('purple');
-    //    cy.get("[type='checkbox']").first().check();
-
+    it.only("Find check-box", () => {
+        cy.get("[for='black']").click()
     });
 });
