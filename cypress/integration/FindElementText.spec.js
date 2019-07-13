dd0d4169e4e89654531d92aec0a79343f1119b8d
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 19:09:51 +0200

    Fix test and add missing expectation
    There's no need to test default browser behavior, this is why we
    just test that the elemenet exist.

--- cypress/integration/FindElementText.spec.js
+++ cypress/integration/FindElementText.spec.js
@@ -2,8 +2,8 @@ describe("Find Text", () => {
     beforeEach(() => cy.visit("buscaelementos/textos"));
 
     it.only("Find Text ", () => {
-   //    cy.get("[href='https://medium.com/automa%C3%A7%C3%A3o-com-batista/debugando-com-o-byebug-2257ae37c3e']").click();
-        cy.get(".btn-floating.halfway-fab.waves-light.red").first().click();
+        cy.get("[href='https://medium.com/automa%C3%A7%C3%A3o-com-batista/debugando-com-o-byebug-2257ae37c3e']")
+            .should("exist");
     })
 
 
