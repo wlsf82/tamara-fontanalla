7a213f4082ddc8e70a552553d786c6fd61075388
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:30:31 +0200

    Update js alert tests

--- cypress/integration/ChangeOfFocusValidationOfAlerts.spec.js
+++ cypress/integration/ChangeOfFocusValidationOfAlerts.spec.js
@@ -2,22 +2,33 @@ describe("JavaScript Alerts", () => {
     beforeEach(() => cy.visit("/mudancadefoco/alert"));
 
     it("Js Alert", () => {
-        cy.contains('Clique para JS Alert').click();
-        //Como ativar a apresentação do alerta no cypress?
+        /**
+         * Como ativar a apresentação do alerta no cypress?
+         * Cypress automatically accepts alerts but you can still assert on the text content.
+         * See https://docs.cypress.io/api/events/catalog-of-events.html#Window-Alert
+        */
+        const stub = cy.stub();
+
+        cy.on("window:alert", stub)
+
+        cy.contains("Clique para JS Alert").click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith("Eu sou um alerta JS!");
+        });
+
+        cy.contains("Você clicou no alerta com sucesso!!");
     });
 
     it("Js Confirm", () => {
-        cy.contains('Clique para JS Confirm').click();
+        cy.contains("Clique para JS Confirm").click();
 
+        cy.contains("Você clicou: Ok");
     });
 
-    it("Js Prompt", () => {
-        //Como preencher o prompt?
-        cy.contains('Clique para JS Prompt').click();
-        cy.window().then((win) => {
-            cy.stub(win, 'prompt').returns('what i want typed into it');
-            cy.get('#save-changes-in-gui-button').click();
-        });
+    it.skip("Js Prompt", () => {
+        /** Como preencher o prompt?
+         * I don't know.
+        */
+        cy.contains("Clique para JS Prompt").click();
     });
     
 });
