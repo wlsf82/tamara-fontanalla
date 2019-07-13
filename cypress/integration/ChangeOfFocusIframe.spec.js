3c8890b3071017656773b49e8c2bcc3c59e164d4
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:03:52 +0200

    Simplify code and adjust indentation

--- cypress/integration/ChangeOfFocusIframe.spec.js
+++ cypress/integration/ChangeOfFocusIframe.spec.js
@@ -2,10 +2,8 @@ describe("Working with Iframe", () => {
     beforeEach(() => cy.visit("mudancadefoco/iframe"));
 
     it("First item in the list", () => {
-        cy.get("#id_do_iframe").then(function($iframe){
-        var b = $iframe.contents().find("#collapsible-header")
-        b.click();
-
+        cy.get("#id_do_iframe").then($iframe => {
+            $iframe.contents().find("#collapsible-header").click();
         });
     });
 });
