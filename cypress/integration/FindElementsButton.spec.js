9cfaf04d215c4a4d1e8f8a078cc9b625f3d27538
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:57:16 +0200

    Separate action and expectation with an emtpy line in betweeen.
    Also, add a empty line between tests.

--- cypress/integration/FindElementsButton.spec.js
+++ cypress/integration/FindElementsButton.spec.js
@@ -3,23 +3,28 @@ describe("Find elements Buttons", () => {
 
     it("Button Raised", () => {
        cy.get("[onclick='ativaedesativa1()']").click();
-       cy.get("#div1 > h5").first().should("contain", "Você Clicou no Botão!");
+
+       cy.contains("Você Clicou no Botão!");
     });
 
     it("Button Floating", () => {
         cy.get(".btn-floating.btn-large.waves-light.red").click();
-        cy.get("#div2 > h5").first().should("contain", "Você Clicou no Botão!");
+
+        cy.contains("Você Clicou no Botão!");
     });
 
     it("Button Flat", () => {
         cy.get(".waves-teal.btn-flat").click();
-        cy.get("#div3 > h5").first().should("contain", "Você Clicou no Botão!");
+
+        cy.contains("Você Clicou no Botão!");
     });
+
     it("Button Submmit", () => {
         cy.get("[onclick='ativaedesativa4()']").click();
-        cy.get("#div4 > h5").first().should("contain", "Você Clicou no Botão!");
-    
+
+        cy.contains("Você Clicou no Botão!");
     });
+
     it("Button Disable", () => {
         //Existe este tipo de teste?
     });
