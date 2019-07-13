e79a3a4b2055789df214e9dde69bbfdad5fd6279
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:50:34 +0200

    Add expectations on tests
    Also, separate action and expectation with an emtpy line in between

--- cypress/integration/FindElementLinks.spec.js
+++ cypress/integration/FindElementLinks.spec.js
@@ -3,18 +3,25 @@ describe("Find Elements Links", () => {
 
     it("Links - OK 200 SUCCESS", () => {
         cy.get("[href='/buscaelementos/success']").click();
-        cy.get("h5").should("contain", "Success!")
+
+        cy.contains("Success!!");
     });
 
     it("Links Erro 400 - Bad Request", () => {
         cy.get("[href='/buscaelementos/badrequest']").click();
+
+        cy.contains("Bad Request!!");
     });
 
     it("Links Erro 404 - Page not found", () => {
         cy.get("[href='/buscaelementos/notfound']").click();
+
+        cy.contains("Page Not Found!!");
     });
 
     it("Links Erro 500 - Internal Server Error", () => {
         cy.get("[href='/buscaelementos/internalservererror']").click();
+
+        cy.contains("Internal Server Error!!");
     });
 });
