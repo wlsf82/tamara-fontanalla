4f90a1939779e47d7503ba2fc168c237dad18a1f
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:06:54 +0200

    Simplify modal test

--- cypress/integration/ChangeOfFocusModal.spec.js
+++ cypress/integration/ChangeOfFocusModal.spec.js
@@ -2,7 +2,7 @@ describe("Change Of Focus Window", () => {
     beforeEach(() => cy.visit("/mudancadefoco/modal"));
 
     it("Open new Modal", () => {
-        cy.get("a.waves-light.btn.modal-trigger").click();
-        cy.get(".modal-close.waves-green.btn-flat").click();
+        cy.contains("Abrir").click();
+        cy.contains("Fechar").click();
     });
 });
