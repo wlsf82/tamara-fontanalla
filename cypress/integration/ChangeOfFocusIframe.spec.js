23626cff5f11989d89e86ff28a74b9b836018fcd
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:07:48 +0200

    Add missing slash on url
    Just to keep the standard.

--- cypress/integration/ChangeOfFocusIframe.spec.js
+++ cypress/integration/ChangeOfFocusIframe.spec.js
@@ -3,7 +3,7 @@
  * See https://docs.cypress.io/guides/references/known-issues.html#Iframes
  */
 describe("Working with Iframe", () => {
-    beforeEach(() => cy.visit("mudancadefoco/iframe"));
+    beforeEach(() => cy.visit("/mudancadefoco/iframe"));
 
     it("First item in the list", () => {
         cy.get("#id_do_iframe").then($iframe => {
