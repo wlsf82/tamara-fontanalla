b4f6afe4ff7810581865dfff89821b901be1bc2c
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 19:22:22 +0200

    Fix mouse hover test

--- cypress/integration/IterationsMouseHover.spec.js
+++ cypress/integration/IterationsMouseHover.spec.js
@@ -1,8 +1,10 @@
 describe("Iterations Mouse Hover", () => {
     beforeEach(() => cy.visit("/iteracoes/mousehover"));
     
-    it(" Mouse Hover", () => {
-        cy.get(".activator").click();
-        cy.get("p").should("contain", "Você usou o mouse hover!");
+    it("Mouse Hover", () => {
+        cy.get(".activator").trigger('mouseover')
+
+        cy.contains("Parábens").should("be.visible");
+        cy.contains("Você usou o mouse hover!").should("be.visible");
     });
 });
