2bfeaa0a3a46a190629b3e100971f05964e1ddbc
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 18:43:46 +0200

    Update opens on new tab test
    Also, add explanation in a comment.

--- cypress/integration/ChangeOfFocusWindow.spec.js
+++ cypress/integration/ChangeOfFocusWindow.spec.js
@@ -1,11 +1,18 @@
 describe("Change Of Focus Window", () => {
     beforeEach(() => cy.visit("mudancadefoco/janela"));
    
-    it.only("Open new window", () => {
-        // nao entendi como validar a abertura de uma nova aba, exemplo no documento:
-        //https://docs.cypress.io/guides/references/trade-offs.html#Inside-the-browser
-     
-        cy.get('a[href="/mudancadefoco/newwindow"]').should('target', '_blank')
-
+    it("Opens on a new tab", () => {
+        /**
+         * Nao entendi como validar a abertura de uma nova aba.
+         * O comportamento padrão de qualquer navegador web é que
+         * se um elemento to tipo anchor o qual possua um atributo
+         * target igual ao valor '_blank', este irar abrir o link
+         * em uma nova aba. Sem este valor o link é aberto na mesma
+         * página.
+         * Portanto, basta testar que o link possui o valor '_blank'
+         * para o atributo 'target' do link.
+         */
+        cy.contains("Clique aqui").invoke("attr", "target")
+            .should("be.eq", "_blank");
     });
 });
