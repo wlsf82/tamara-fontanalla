bf1df61532c90b54c837ed81ec73c213c243fa56
Walmyr Filho <limaesilva@qelp.com>
2019-07-13 19:02:04 +0200

    Add missing spaces on expecation
    This improves readability.

--- cypress/integration/FindElementTable.spec.js
+++ cypress/integration/FindElementTable.spec.js
@@ -2,6 +2,6 @@ describe("Find element table", () => {
     beforeEach(() => cy.visit("buscaelementos/table"));
    
     it("First item in the list", () => {
-        cy.get('td').should('contain', 'Arroz','1','$2.87');
+        cy.get('td').should('contain', 'Arroz', '1', '$2.87');
     });
 });
