3767fe641d60cb8c612220588822f841eb1d40e2
Walmyr Filho <limaesilva@qelp.com>
2019-07-14 02:21:05 +0200

    Suggestion of test solution

--- cypress/integration/Userlist.spec.js
+++ cypress/integration/Userlist.spec.js
@@ -1,11 +1,24 @@
 describe("User List", () => {
     beforeEach(() => cy.visit("/users"));
     
-    it("View user's profile", () => {
-        cy.get(".material-icons").first().click();
-        // Neste teste o que pode ser utilizado para comparar os dados que são apresentados?
-
+    it.only("View user's profile", () => {
+        /**
+         * Neste teste o que pode ser utilizado para comparar os dados que são apresentados?
+         * Veja a solução abaixo:
+        */
+        cy.get('tr td')
+            .first()
+            .invoke('text')
+            .then(name => {
+                cy.get(".material-icons")
+                    .first()
+                    .click();
+                cy.get("div.col.s12.center p")
+                    .first()
+                    .should("contain", name);
+            });
     });
+
     it("change first user's email", () => {
         cy.get('.material-icons').contains('edit').first().click();
         cy.get("#user_email").clear();
@@ -17,6 +30,7 @@ describe("User List", () => {
 
         cy.get("#notice").should("contain", "Seu Usuário foi Atualizado!");
     });
+
     it("delete first user", () => {
         cy.get("[data-method='delete']").first().click();
         cy.get("#notice").should("contain", "Seu Usuário foi removido com sucesso!");
