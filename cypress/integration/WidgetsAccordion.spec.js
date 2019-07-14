b0bdb70c0638497a9c1207f08986abead9610128
Walmyr Filho <limaesilva@qelp.com>
2019-07-14 02:23:01 +0200

    Add empty lines between tests
    This improves the readability.

--- cypress/integration/WidgetsAccordion.spec.js
+++ cypress/integration/WidgetsAccordion.spec.js
@@ -5,10 +5,12 @@ describe("Change Of Focus Window", () => {
         cy.get(".material-icons").first().click();
         cy.get(".collapsible-body").should("contain", "Primeiro Texto.");
     });
+
     it("Open Second", () => {
         cy.contains('Second').click();
         cy.get(".collapsible-body").should("contain", "Segundo Texto.");
     });
+
     it("Open Third", () => {
         cy.contains('Third').click();
         cy.get(".collapsible-body").should("contain", "Terceiro Texto.");
