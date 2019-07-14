daba0b4dc2ac7f46c417650038ab26aaf8460db0
Walmyr Filho <limaesilva@qelp.com>
2019-07-14 03:04:48 +0200

    Delete non-working tests

--- cypress/integration/OthersAuth.spec.js
+++
@@ -1,7 +0,0 @@
-describe("Others Auth", () => {
-    beforeEach(() => cy.visit("/outros/uploaddearquivos"));
-
-    it("Others Auth", () => {
-        //Aqui nao é possível inspecionar um elemento, neste momento o JS Prompt é apresentado
-    });
-});
--- cypress/integration/OthersUploadFile.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Upload File", () => {
-    beforeEach(() => cy.visit("/outros/uploaddearquivos"));
-
-    it("Upload File", () => {
-        cy.get(".file-field.input-field").click();
-        //Aqui o click nao realiza a abertura da tela para selecao do local do arquivo
-    });
-});
